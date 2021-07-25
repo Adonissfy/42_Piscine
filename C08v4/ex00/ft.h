d1cc073b3483fa855baab7e673d062e5e8172c06
Adonissfy <86737350+Adonissfy@users.noreply.github.com>
2021-07-25 20:11:41 +0200

    8

---
+++ C08v4/ex00/ft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaffroy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/22 18:55:39 by asaffroy          #+#    #+#             */
+/*   Updated: 2021/07/22 19:00:56 by asaffroy         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+#endif
