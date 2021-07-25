2d64716800b8f6dd2dde4914051c4d99961ffd4b
Adonissfy <86737350+Adonissfy@users.noreply.github.com>
2021-07-25 20:11:10 +0200

    7

---
+++ C07v3/ex02/ft_ultimate_range.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaffroy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/20 08:19:39 by asaffroy          #+#    #+#             */
+/*   Updated: 2021/07/22 13:57:45 by asaffroy         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	a;
+
+	a = 0;
+	if (min >= max)
+	{
+		range = NULL;
+		return (0);
+	}
+	*range = malloc(sizeof(int *) * (max - min));
+	if (!*range)
+		return (-1);
+	while (min < max)
+		(*range)[a++] = min++;
+	return (a);
+}
