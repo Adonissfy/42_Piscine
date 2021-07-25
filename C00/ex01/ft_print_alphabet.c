0960000036b81457bf8993703ae1df68ad09998d
Adonissfy <86737350+Adonissfy@users.noreply.github.com>
2021-07-25 20:08:21 +0200

    2

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaffroy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/07 14:58:54 by asaffroy          #+#    #+#             */
+/*   Updated: 2021/07/07 15:20:43 by asaffroy         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void);
+
+void	ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
