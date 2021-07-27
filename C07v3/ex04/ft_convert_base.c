285d5d7a4d6d3e860154c81ef5464737ddd3ad3c
Adonissfy <86737350+Adonissfy@users.noreply.github.com>
2021-07-27 12:04:02 +0200

    Add files via upload

--- C07v3/ex04/ft_convert_base.c
+++ C07v3/ex04/ft_convert_base.c
@@ -5,120 +5,83 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: asaffroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/22 11:41:53 by asaffroy          #+#    #+#             */
-/*   Updated: 2021/07/22 20:17:11 by asaffroy         ###   ########lyon.fr   */
+/*   Created: 2021/07/27 08:58:17 by asaffroy          #+#    #+#             */
+/*   Updated: 2021/07/27 09:52:54 by asaffroy         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdlib.h>
 
-long int	ft_pow(int nb, int power);
-int			ft_len(char *c);
-
-int	ft_atoi(char *nb, char *base, long int i)
+int	ft_strlen(char *str)
 {
-	long int	j;
-	long int	a;
-	long int	m;
+	int		i;
 
-	m = 1;
-	a = 0;
-	while ((nb[i] >= 9 && nb[i] <= 13) || nb[i] == ' ')
+	i = 0;
+	while (str[i])
 		i++;
-	while (nb[i] == '+' || nb[i] == '-')
-	{
-		if (nb[i] == '-')
-			m = m * -1;
-		i++;
-	}
-	while (nb[i])
-	{
-		j = 0;
-		while (nb[i] != base[j] && base[j])
-			j++;
-		if (nb[i] != base[j])
-			return (a);
-		a = ft_len(base) * a + j;
-		i++;
-	}
-	return (a * m);
+	return (i);
 }
 
-int	ft_test_base(char *base)
+void	ft_rev_tab(char *tab, int size)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		swap;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		swap = tab[size - 1 - i];
+		tab[size - i - 1] = tab[i];
+		tab[i] = swap;
+		i++;
+	}
+}
+
+int	i_base(char c, char *base)
+{
+	int		i;
+
+	i = 0;
+	while (base[i] && base[i] != c)
+		i++;
+	return (i);
+}
+
+int	is_base(char c, char *base)
+{
+	int		i;
 
 	i = 0;
 	while (base[i])
 	{
-		j = 0;
-		if ((base[i] >= 9 && base[i] <= 13) || base[i] == '-' || base[i] == '+')
+		if (base[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_base(char *base)
+{
+	int		i;
+	int		j;
+	int		len;
+
+	i = 0;
+	len = ft_strlen(base);
+	if (len == 0 || len == 1)
+		return (0);
+	while (base[i])
+	{
+		j = i + 1;
+		if (base[i] == '-' || base[i] == '+' || base[i] < 32
+			|| base[i] > 127 || base[i] == ' ')
 			return (0);
 		while (base[j])
 		{
-			if (base[i] == base[j] && i != j)
+			if (base[i] == base[j])
 				return (0);
 			j++;
 		}
 		i++;
 	}
-	if (i < 2)
-		return (0);
 	return (1);
-}
-
-char	*ft_to_base(long int nbr, char *base, int size, int sign)
-{
-	int				j;
-	int				i;
-	long int		res;
-	char			*tab;
-
-	i = size;
-	j = ft_len(base);
-	tab = malloc((size + sign + 1));
-	if (!tab)
-		return (0);
-	if (sign == 1)
-	{
-		tab[0] = '-';
-		nbr = -nbr;
-		i--;
-	}
-	sign--;
-	while (++sign < size)
-	{
-		res = nbr / ft_pow(j, --i);
-		res = res % j;
-		tab[sign] = base[res];
-	}
-	tab[sign] = '\0';
-	return (tab);
-}
-
-char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
-{
-	long int		b10;
-	long int		i;
-	int				sign;
-	char			*tab;
-
-	if (ft_test_base(base_from) == 0 || ft_test_base(base_to) == 0)
-		return ("\0");
-	sign = 0;
-	b10 = ft_atoi(nbr, base_from, 0);
-	i = 1;
-	while ((b10 / i > 0 && b10 > 0) || (b10 / i < 0 && b10 < 0))
-	{
-		i = i * ft_len(base_to);
-		sign++;
-	}
-	i = sign;
-	if (b10 < 0)
-		tab = ft_to_base(b10, base_to, ++i, 1);
-	else if (b10 == 0)
-		tab = ft_to_base(b10, base_to, 1, 0);
-	else
-		tab = ft_to_base(b10, base_to, i, 0);
-	return (tab);
 }
--- C07v3/ex04/ft_convert_base2.c
+++ C07v3/ex04/ft_convert_base2.c
@@ -5,36 +5,88 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: asaffroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/22 11:43:02 by asaffroy          #+#    #+#             */
-/*   Updated: 2021/07/22 11:43:22 by asaffroy         ###   ########lyon.fr   */
+/*   Created: 2021/07/27 09:47:05 by asaffroy          #+#    #+#             */
+/*   Updated: 2021/07/27 09:59:51 by asaffroy         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-long int	ft_pow(int nb, int power)
-{
-	int	i;
-	int	res;
+#include <stdlib.h>
 
-	i = 1;
-	res = nb;
-	if (power < 0)
+int		ft_strlen(char *str);
+int		i_base(char c, char *base);
+int		is_base(char c, char *base);
+int		check_base(char *base);
+void	ft_rev_tab(char *tab, int size);
+
+long int	ft_atoi_base(char *str, char *base)
+{
+	long int	atoi;
+	int			negative;
+
+	atoi = 0;
+	negative = 1;
+	if (!check_base(base))
 		return (0);
-	if (power == 0)
-		return (1);
-	while (i < power)
+	while (*str == '\t' || *str == '\v' || *str == '\n' || *str == '\r'
+		|| *str == '\f' || *str == ' ')
+		str++;
+	while (*str == '+' || *str == '-')
 	{
-		res *= nb;
-		i++;
+		if (*str == '-')
+			negative = negative * -1;
+		str++;
 	}
-	return (res);
+	while (is_base(*str, base))
+	{
+		atoi = atoi * ft_strlen(base) + i_base(*str, base);
+		str++;
+	}
+	return (atoi * negative);
 }
 
-int	ft_len(char *s)
+long int	ft_fill_tab(char *tab, long int num, char *base_to)
 {
-	int	i;
+	int			negative;
+	int			base_len;
+	long int	i;
+
+	base_len = ft_strlen(base_to);
+	negative = 0;
+	i = 0;
+	if (num < 0)
+	{
+		negative = 1;
+		num = -num;
+	}
+	if (num == 0)
+	{
+		tab[i++] = base_to[num % base_len];
+	}
+	while (num)
+	{
+		tab[i++] = base_to[num % base_len];
+		num = num / base_len;
+	}
+	if (negative)
+		tab[i++] = '-';
+	return (i);
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	long int	num;
+	int			i;
+	char		*tab;
 
 	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+	if (!check_base(base_from) || !check_base(base_to))
+		return (NULL);
+	tab = malloc(sizeof(char) * 34);
+	if (!tab)
+		return (NULL);
+	num = ft_atoi_base(nbr, base_from);
+	i = ft_fill_tab(tab, num, base_to);
+	ft_rev_tab(tab, i);
+	tab[i] = '\0';
+	return (tab);
 }
